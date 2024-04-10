08fc5acef73e943a6a808a8982f613d2ca107c19
Karim Mehwechi <mehwechikarim@gmail.com>
2024-04-10 16:55:55 +0100

    Implements event planner crud

--- src/main/java/com/esprit/microservices/eventsplanner/EventsplannerApplication.java
+++ src/main/java/com/esprit/microservices/eventsplanner/EventsplannerApplication.java
@@ -2,8 +2,10 @@ package com.esprit.microservices.eventsplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EventsplannerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/esprit/microservices/eventsplanner/services/EventServiceImpl.java
+++ src/main/java/com/esprit/microservices/eventsplanner/services/EventServiceImpl.java
@@ -1,2 +1,80 @@
-package com.esprit.microservices.eventsplanner.services;public class EventServiceImpl {
+package com.esprit.microservices.eventsplanner.services;
+
+import com.esprit.microservices.eventsplanner.entities.Event;
+import com.esprit.microservices.eventsplanner.interfaces.IEventService;
+import com.esprit.microservices.eventsplanner.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EventServiceImpl implements IEventService {
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @Override
+    public List<Event> retrieveAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    @Override
+    public Event addEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public Event updateEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public Optional<Event> retrieveEvent(Integer idEvent) {
+        return eventRepository.findById(idEvent);
+    }
+
+    @Override
+    public void removeEvent(Integer idEvent) {
+        eventRepository.deleteById(idEvent);
+    }
+
+    @Override
+    public List<Event> retrieveEventsByTitle(String title) {
+        return eventRepository.findByTitle(title);
+    }
+
+    @Override
+    public List<Event> retrieveEventsByStatus(String status) {
+        return eventRepository.findByStatus(status);
+    }
+
+    @Override
+    public List<Event> retrieveEventsByDate(LocalDate date) {
+        return eventRepository.findByStartDate(date);
+    }
+
+    @Override
+    public List<Event> retrieveEventsByType(String type) {
+        return eventRepository.findByType(type);
+    }
+
+    @Override
+    public List<Event> retrieveEventsByLocation(String location) {
+        return eventRepository.findByLocation(location);
+    }
+
+    @Override
+    public List<Event> retrieveEventsBetweenTwoDates(LocalDate startDate, LocalDate endDate) {
+        return eventRepository.getEventBetweenTwoDates(startDate, endDate);
+    }
+
+    @Override
+    public List<Event> retrieveEventsByNumberOfAttendees(Integer numberOfAttendeesCount) {
+        return eventRepository.findByNumberOfAttendees(numberOfAttendeesCount);
+    }
+
+
 }
